9a43b8ae6b3576a2f75173ea60a978a8d09ffa86
beauttie <bak02013@pomona.edu>
2020-10-07 12:24:22 -0700

    Brainstorm for show_details method

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -4,11 +4,12 @@ require_relative 'user'
 require_relative 'channel'
 
 class Workspace
-  attr_reader :users, :channels
+  attr_reader :users, :channels, :selected
 
-  def initialize
+  def initialize(selected = nil)
     @users = User.list_all
     @channels = Channel.list_all
+    @selected = selected
   end
 
   def list_users
@@ -20,14 +21,15 @@ class Workspace
   end
 
   def select_user(id_or_name)
-    return @users.select { |user| user.slack_id == id_or_name || user.name == id_or_name }
+    @selected = @users.select { |user| user.slack_id == id_or_name || user.name == id_or_name }
   end
 
   def select_channel(id_or_name)
-    return @channels.select { |channel| channel.slack_id == id_or_name || channel.name == id_or_name }
+    @selected = @channels.select { |channel| channel.slack_id == id_or_name || channel.name == id_or_name }
   end
 
   def show_details
+    # check value of @selected
     # tp select_user(id_or_name), "slack_id", "name", "real_name", "status_text", "status_emoji"
     # tp select_channel(id_or_name), "slack_id", "name", "topic", "member_count"
   end
