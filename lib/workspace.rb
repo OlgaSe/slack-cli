a74b1dbae9a9c1816e4235c8eca25afe882ff735
beauttie <bak02013@pomona.edu>
2020-10-08 00:45:41 -0700

    Add test for Channel class

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -27,17 +27,19 @@ class Workspace
   def select_user(id_or_name)
     @selected = @users.find { |user| user.slack_id == id_or_name || user.name == id_or_name }
 
-    raise ArgumentError, "User with that name or Slack ID not found" if no_recipient
+    raise ArgumentError, "User with that name or Slack ID not found." if no_recipient
+    return "User selected."
   end
 
   def select_channel(id_or_name)
     @selected = @channels.find { |channel| channel.slack_id == id_or_name || channel.name == id_or_name }
 
-    raise ArgumentError, "Channel with that name or Slack ID not found" if no_recipient
+    raise ArgumentError, "Channel with that name or Slack ID not found." if no_recipient
+    return "Channel selected."
   end
 
   def show_details
-    raise NoMethodError, "No recipient is currently selected" if no_recipient
+    raise NoMethodError, "No recipient is currently selected." if no_recipient
     return @selected.details
   end
 
--- test/channel_test.rb
+++ test/channel_test.rb
@@ -1,1 +1,56 @@
 require_relative 'test_helper'
+require_relative '../lib/channel'
+
+describe "Channel class" do
+  describe "Channel instantiation" do
+    before do
+      @channel = Channel.new("1", "Test Channel", "Anything", 2)
+    end
+
+    it "is an instance of Channel" do
+      expect(@channel).must_be_kind_of Channel
+    end
+
+    it "is set up for specific attributes and data types" do
+      [:details, :list_all].each do |prop|
+        expect(@channel).must_respond_to prop
+      end
+
+      expect(@channel.slack_id).must_be_kind_of String
+      expect(@channel.name).must_be_kind_of String
+      expect(@channel.topic).must_be_kind_of String
+      expect(@channel.status).must_be_kind_of Integer
+    end
+  end
+
+  describe "list_all method" do
+    it "returns an Array of Channels" do
+      VCR.use_cassette("list_channels") do
+        channels_list = Channel.list_all
+
+        expect(channels_list).must_be_kind_of Array
+        expect(channels_list.all?(Channel)).must_equal true
+
+        expect(channels_list.length).must_equal 3
+      end
+    end
+  end
+
+  describe "details method" do
+    before do
+      @channel = Channel.new("1", "Test Channel", "Anything", 2)
+    end
+
+    it "returns a String including specific attributes" do
+      channel_details = @channel.details
+
+      expect(channel_details).must_be_kind_of String
+
+      expect(channel_details).must_include @channel.slack_id
+      expect(channel_details).must_include @channel.name
+      expect(channel_details).must_include @channel.topic
+      expect(channel_details).must_include @channel.member_count
+    end
+  end
+
+end
