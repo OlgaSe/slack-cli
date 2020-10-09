099fd466faed2447f7389c59bd55f55945f31c4b
beauttie <bak02013@pomona.edu>
2020-10-08 21:08:40 -0700

    Modify output of select methods to include Slack ID or name

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -28,14 +28,14 @@ class Workspace
     @selected = @users.find { |user| user.slack_id == id_or_name || user.name == id_or_name }
 
     raise ArgumentError, "User with that name or Slack ID not found." if no_recipient
-    return "User selected."
+    return "User #{id_or_name} selected."
   end
 
   def select_channel(id_or_name)
     @selected = @channels.find { |channel| channel.slack_id == id_or_name || channel.name == id_or_name }
 
     raise ArgumentError, "Channel with that name or Slack ID not found." if no_recipient
-    return "Channel selected."
+    return "Channel #{id_or_name} selected."
   end
 
   def show_details
